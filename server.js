2665e1bd450dcf36602fa04516e730dcd11f6c1d
Adam Shafer <adam@adamshafer.com>
2021-06-14 15:55:05 -0500

    fixed server.js config

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const history = require("connect-history-api-fallback");
 const enforce = require("express-sslify");
 
 const app = express();
-app.use(enforce.HTTPS({trustProtoHeader: true}));
-app.use(serveStatic(__dirName + "/dist"));
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
-app.listen(process.env.port || 5000);
+
+app.listen(process.env.PORT || 5000);
